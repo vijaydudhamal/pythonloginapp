67b13dfda6ea2d4006c0cd4c7e56b0c7bcc58d38
vijaydudhamal <165992267+vijaydudhamal@users.noreply.github.com>
2025-10-08 14:28:55 +0530

    update  commit

--- Terraform/beckend.tf
+++ Terraform/beckend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm"{
-    resource_group_name   = "kml_rg_main-c41d078df6ac415a"
+    resource_group_name   = "kml_rg_main-bfd4c4f26f0b42d7"
     storage_account_name  = "backendtffile123575"
     container_name        = "prod-tfstate"
     key                   = "prod.terraform.tfstate"
